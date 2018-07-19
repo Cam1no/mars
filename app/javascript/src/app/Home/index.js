d9447b804750867306dd7cebe687c5eb9a0bd0ae
joe <kinjoyuji@gmail.com>
2018-07-20 01:52:09 +0900

    Hello buluma

--- app/javascript/src/app/Home/index.js
+++ app/javascript/src/app/Home/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Columns} from 'react-bulma-components';
 // import { connect } from 'react-redux';
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return(
-      <div>
-        HelloWorld
-      </div>
+	render() {
+		return (
+      <Columns>
+        {
+          [1, 2, 3, 4, 5].map(i => {
+            return(
+              <Columns.Column>
+        				First Column {`${i}`}
+        			</Columns.Column>
+            )
+          })
+        }
+		  </Columns>
     )
-  }
+	}
 }
 
 export default Home;
