01dfad2cc88e7898bbca569e9b345cd553438427
joe <kinjoyuji@gmail.com>
2018-07-22 03:22:09 +0900

    fix deploy setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :rbenv_roles, :all
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w[rake gem bundle ruby rails]
-set :branch, 'master'
+set :branch, ENV['BRANCH'] || 'master'
 set :linked_dirs, fetch(:linked_dirs, []).push(
   'log',
   'tmp/pids',
