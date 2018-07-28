911e84019d321d8307a67cc6bb254edf47cb3fee
joe <kinjoyuji@gmail.com>
2018-07-29 01:21:24 +0900

    bundle exec annotate --routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@
 #
 #                    Prefix Verb URI Pattern                                                                              Controller#Action
 #                      root GET  /                                                                                        react#root
+#               user_photos GET  /users/:user_id/photos(.:format)                                                         user/photos#index
 #                     users GET  /users(.:format)                                                                         users#index
+#                    photos GET  /photos(.:format)                                                                        photos#index
 #        rails_service_blob GET  /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
 # rails_blob_representation GET  /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
 #        rails_disk_service GET  /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
